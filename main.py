29c123c1090faf95eeb3a8ea8c699fd979427f26
alphadzingina <alphaadx6@gmail.com>
2022-06-05 02:47:16 +0100

    generate password

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from email.mime import image
 from tkinter import *
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+from random import choice, randint, shuffle
 
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -29,11 +44,11 @@ website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "alphaadx6@gmail.com ")
-password_entry = Entry(width=35)
+password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1, columnspan=2)
 
 # Buttons
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command="generate_password")
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=30)
 add_button.grid(row=4, column=1, columnspan=2)
